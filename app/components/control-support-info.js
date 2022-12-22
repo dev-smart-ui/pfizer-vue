10b66ad77985456a3f105db278e8b48fd8a0c9d3
VHromyk <108476400+dev-smart-ui@users.noreply.github.com>
2022-12-22 13:23:28 +0200

    added variable to the supporting information

--- app/components/control-support-info.js
+++ app/components/control-support-info.js
@@ -1,17 +1,15 @@
-app.component('supporting-info', {
-     props: ['id'],
-     data() {
-         return {
-            questionsList: [window.Q1, window.Q2, window.Q3, window.Q4],
-
-         }
-     },
-     template: `      
+app.component("supporting-info", {
+  props: ["id"],
+  data() {
+    return {
+      questionsList: [window.Q1, window.Q2, window.Q3, window.Q4],
+      data: window.BASE_INFO,
+    };
+  },
+  template: `      
         <div class="q-info-holder">
-            <h4>Supporting information</h4>
+            <h4>{{ data.supportingInfo }}</h4>
             <div v-html='questionsList[id].supportInfo'></div>            
         </div>
-     `
- })
-
-
+     `,
+});
--- app/data/page-data/baseInfo.js
+++ app/data/page-data/baseInfo.js
@@ -1,13 +1,14 @@
 window.BASE_INFO = {
-    questionRemaining: 'question remaining',
-    resetBtn: 'Reset',
-    viewAnswerBtn: 'View Answer',
-    addInfoBtn: 'Additional Information',
-    nextQuestionBtn: 'Next Question',
-    yourAnswer: 'Your answer',
-    stillHere: 'STILL HERE?',
-    tapToContinue: 'Tap to continue',
-    exitSession: 'Exit session',
-    loading: 'Loading',
-    correctAnswer: 'Correct answer'
-}
+  questionRemaining: "question remaining",
+  resetBtn: "Reset",
+  viewAnswerBtn: "View Answer",
+  addInfoBtn: "Additional Information",
+  supportingInfo: "Supporting information",
+  nextQuestionBtn: "Next Question",
+  yourAnswer: "Your answer",
+  stillHere: "STILL HERE?",
+  tapToContinue: "Tap to continue",
+  exitSession: "Exit session",
+  loading: "Loading",
+  correctAnswer: "Correct answer",
+};
